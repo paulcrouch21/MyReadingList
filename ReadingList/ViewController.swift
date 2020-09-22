442aafe82844a1c3204b5df9edd612cdb8230180
paulcrouch99 <paul.crouch99@yahoo.com>
2020-09-21 20:01:17 -0500

    First commit

---
+++ ReadingList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReadingList
+//
+//  Created by Paul Crouch on 9/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
