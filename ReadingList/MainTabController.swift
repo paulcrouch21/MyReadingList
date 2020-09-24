5a8e7336c14badaf75c3a0f5cb7a0ada9d710658
paulcrouch99 <paul.crouch99@yahoo.com>
2020-09-23 20:29:40 -0500

    Set the UI for the Settings tab

---
+++ ReadingList/MainTabController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabController.swift
+//  ReadingList
+//
+//  Created by Paul Crouch on 9/23/20.
+//
+
+import Foundation
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ ReadingList/SettingsTableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsTableViewController.swift
+//  ReadingList
+//
+//  Created by Paul Crouch on 9/23/20.
+//
+
+import Foundation
+import UIKit
+
+class SettingsTableViewController : UITableViewController {
+    
+    //MARK: Properties
+    
+    @IBOutlet weak var aboutCell: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
